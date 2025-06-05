026123dac7c56ce596447748abb5d6a57b1f24bc
Vinsensius <tyouthvinsen@gmail.com>
2025-06-05 15:22:21 +0700

    Initial commit for 3Our

---
+++ App.tsx
@@ -0,0 +1,28 @@
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Services from './components/Services';
+import AboutUs from './components/AboutUs';
+import Portfolio from './components/Portfolio';
+import CallToAction from './components/CallToAction';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <main>
+        <Hero />
+        <Services />
+        <AboutUs />
+        <Portfolio />
+        <CallToAction />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
